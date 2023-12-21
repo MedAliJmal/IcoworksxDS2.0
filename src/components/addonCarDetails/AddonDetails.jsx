9461b8507f8bd25a22dbb9a74953dac5bb31b809
MedAliJmal <speth4tunisia@gmail.com>
2023-12-21 22:10:56 +0100

    v4XxXXX

--- src/components/addonCarDetails/AddonDetails.jsx
+++ src/components/addonCarDetails/AddonDetails.jsx
@@ -91,7 +91,7 @@ const AddonDetails = () => {
         <iframe
           width="854"
           height="480"
-          src={`${car.vid}?si=eqQ4CW46MZm-5E1x&autoplay=1&vq=hd720`}
+          src={`${car.vid}?si=eqQ4CW46MZm-5E1x&autoplay=1&vq=hd1080`}
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;fullscreen"
